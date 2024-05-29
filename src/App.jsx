b4cc748d2b4fb2fad6732d8da0339056f5c3b050
Ana Carolina Feliciana <anacarolinam665@gmail.com>
2024-05-29 12:49:27 -0300

    style: posicionamento do card no centro da tela

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
 
+
 import { Home } from "./pages/Home"
 import { theme } from "./styles/theme.js"
-import { GlobalStyle } from "./styles/global.js"
+import { GlobalStyle} from "./styles/global.js"
 
 const router = createBrowserRouter([
   {
